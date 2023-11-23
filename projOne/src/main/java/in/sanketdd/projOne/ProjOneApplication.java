003096c65b737679a4e89b491a0c81b60bc8b96b
team-cores <sanket.deshpande@coreviwsystems.com>
2023-11-23 10:32:43 +0530

    added run method return type

--- projOne/src/main/java/in/sanketdd/projOne/ProjOneApplication.java
+++ projOne/src/main/java/in/sanketdd/projOne/ProjOneApplication.java
@@ -2,12 +2,18 @@ package in.sanketdd.projOne;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ProjOneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjOneApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ProjOneApplication.class, args);
+		System.out.println(SpringApplication.getShutdownHandlers());
+		System.out.println(SpringApplication.BANNER_LOCATION_PROPERTY_VALUE);
+		System.out.println(context.getApplicationStartup());
+		System.out.println(context.getEnvironment().getPropertySources());
+		System.out.println(context.getBeanDefinitionCount());
 	}
 
 }
